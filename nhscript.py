e8d6bdc3304646c3fa5688bfab20284c806ef4da
[REDACTED] <vikee.email@gmail.com>
2020-11-20 17:09:52 +0530

    Removed code used for running tagger standalone.

--- nhscript.py
+++ nhscript.py
@@ -1,8 +1,5 @@
 from bs4 import BeautifulSoup
-import discord
 import requests
-import argparse
-
 
 def print_info(page_id: int, site: str):
     try:
@@ -11,7 +8,6 @@ def print_info(page_id: int, site: str):
         return (f"Requested manga not found on {site}. \n")
 
     return (f'{site}\nYour url: {manga(page_id, site)}\n{data}\n')
-
 
 def manga(page: int, source: str):
     """Returns the correct URL for the given source"""
@@ -22,25 +18,10 @@ def manga(page: int, source: str):
     elif source == 'nyahentai':
         return f"https://nyahentai.com/g/{page}"
 
-
 def fetch_website(url: str):
     """Loading website and returning the HTML"""
     return requests.get(url).text
 
-
 def soupify(soup):
     """Parses HTML into readable format via BeautifulSoup"""
     return BeautifulSoup(soup, 'html.parser')
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("manga_id", help="ID of the manga.", type=int)
-    data = parser.parse_args()
-    manga_id = str(data.manga_id)
-
-    if not manga_id.isnumeric():
-        print(f'moolah ({manga_id}) is not numeric')
-        exit()
-    for x in {'nyahentai', '9hentai', 'nhentai'}:
-        print_info(manga_id, x)
