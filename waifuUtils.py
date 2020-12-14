9dcb794772737a1c14b4147277581f7275f8c4bf
[REDACTED] <vikee.email@gmail.com>
2020-12-14 09:07:48 +0530

    Added weighted random generation of waifus based on their AniList popularity.

--- waifuUtils.py
+++ waifuUtils.py
@@ -1,8 +1,10 @@
+from databaseUtils import *
 from anilistTest import *
 import discord
 from pybooru import Danbooru
 import requests
 import random
+from saucenao_api import SauceNao
 
 def get_waifus(blacklist: list=[]) -> dict:
     """OPTIONAL: blacklist are URL's to not be received."""
@@ -30,7 +32,6 @@ def get_waifu():
     #getting 1 waifu from waifu.pics
     URL = "https://waifu.pics/api/sfw/waifu"
     response = requests.get(url=URL)
-    print (response.json()['url'])
     return response.json()['url']
 
 def get_xwaifu():
@@ -103,7 +104,53 @@ def get_from_danbooru(char_name, anime_name):
     return posts
 
 def generate_random_waifu():
-    while True:
-        n = random.randint(1,5000)
-        waifu_info = get_waifuinfo_id(n)
-        return waifu_info
+    tier_list = ['S']*100 + ['A']*500 + ['B']*1000 + ['C']*1500 + ['D']*1900 #can be modded to change probabilities.
+    tier = random.choice(tier_list)
+    choice = {
+        'S': {
+            "start" : 1,
+            "end" : 100
+        },
+        'A': {
+            "start" : 101,
+            "end" : 600
+        },
+        'B': {
+            "start" : 601,
+            "end" : 1600
+        },
+        'C': {
+            "start" : 1601,
+            "end" : 3100
+        },
+        'D': {
+            "start" : 3101,
+            "end" : 5000
+        }
+    }.get(tier)
+    n = random.randint(choice["start"], choice["end"])
+    
+    
+    
+    #no need to change the code below.
+    waifu_info = get_waifuinfo_id(n)
+    insert_waifu_pos_data(waifu_info["id"], n)
+    waifu_id = waifu_info["id"]
+    print(f"ID:{waifu_id}. Position:{n}")
+    return waifu_info
+
+def get_sauce(url):
+    sauce = SauceNao()
+    return sauce.from_url(url)[0]    
+
+    """
+    for post in posts:
+        try:
+            print(post['large_file_url'])
+            break
+        except:
+            continue
+
+    return posts
+    """
+
