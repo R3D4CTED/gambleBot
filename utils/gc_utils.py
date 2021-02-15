30acdaa383f947a182e95b19ec2835aedb4aa625
[REDACTED] <vikee.email@gmail.com>
2021-02-15 16:23:20 +0530

    Add gclone utils file with the requrired flags for the transfers.

---
+++ utils/gc_utils.py
@@ -0,0 +1,1 @@
+SRflag = ("--transfers 50 -v --stats-one-line --stats=15s --ignore-existing --drive-server-side-across-configs --drive-chunk-size 128M --drive-acknowledge-abuse --drive-keep-revision-forever")
